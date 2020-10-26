4772dd3d5006c7b2fef18fc99ee3b6b30084759b
Yukissss <szwangyuqi@gmail.com>
2020-10-26 04:27:09 -0400

    Site updated: 2020-10-26 04:27:09

---
+++ dist/music.js
@@ -0,0 +1,10 @@
+const ap = new APlayer({
+    container: document.getElementById('player'),
+    fixed: true,
+    audio: [{
+        name: 'Canon in D',
+        artist: 'dylanf',
+        url: 'https://music.163.com/#/song/media/outer/url?id=475218187.mp3',
+        cover: 'https://p1.music.126.net/fL7FAeRby1s7JreBqoOKjg==/109951165175371079.jpg?param=90y90',
+    }]
+});
